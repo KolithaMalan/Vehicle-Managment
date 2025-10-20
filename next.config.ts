d2ea428f2fe02f488b13ef3617948a509dd06330
KolithaMalan <koliyxmalan121@gmail.com>
2025-10-20 22:26:05 +0530

    nextConfig Update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose'],
+  },
+  
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('mongoose');
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
